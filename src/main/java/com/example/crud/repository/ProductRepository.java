b12b664ef1355b2842e7d6542bca57547da6fe3f
Magibalan0073 <magi.m@bmwtechworks.in>
2026-08-12 12:22:26 +0530

    Delete src directory

--- src/main/java/com/example/crud/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud.repository;
-
-import com.example.crud.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
